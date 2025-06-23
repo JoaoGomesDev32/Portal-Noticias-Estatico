961c255659e4f6c35cdcfb3900e1a20715ec7a74
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-06-23 14:39:36 +0100

    :tada: init project and initial config

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  console.log(req.query);
+
+  if (req.query.busca == null) {
+    res.send("Home");
+  } else {
+    res.send(`Você buscou: ${req.query.busca}`);
+  }
+});
+
+app.get("/:slug", (req, res) => {
+  res.send(req.params.slug);
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
